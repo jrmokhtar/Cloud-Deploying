bef376a8c094a39b196584ca99ec4b0eb28b138e
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-04-30 05:39:27 +0300

    Update 2.1.db_instance.tf

--- Terrafrom/2.1.db_instance.tf
+++ Terrafrom/2.1.db_instance.tf
@@ -1,23 +1,23 @@
-# resource "aws_db_instance" "RDS_instance" {
+ resource "aws_db_instance" "RDS_instance" {
  
-#   db_subnet_group_name = aws_db_subnet_group.RDS_subnets.name
-#   vpc_security_group_ids = [aws_security_group.RDS_secgroup.id]
-#   availability_zone = data.aws_availability_zones.AZs.names[0]
+db_subnet_group_name = aws_db_subnet_group.RDS_subnets.name
+   vpc_security_group_ids = [aws_security_group.RDS_secgroup.id]
+   availability_zone = data.aws_availability_zones.AZs.names[0]
   
-#   db_name = "rds"
-#   license_model = "general-public-license"
-#   identifier = "dhangodb"
+   db_name = "rds"
+   license_model = "general-public-license"
+   identifier = "dhangodb"
 
-#   allocated_storage    = 20
-#   engine              = "mysql"
-#   engine_version      = "8.0.35"
-#   instance_class      = "db.t3.micro"
-#   publicly_accessible = true
-#   skip_final_snapshot = true
+   allocated_storage    = 20
+   engine              = "mysql"
+   engine_version      = "8.0.35"
+   instance_class      = "db.t3.micro"
+   publicly_accessible = true
+   skip_final_snapshot = true
 
-#   username            = "jrmokhtar"
-#   password            = "NJ7q0451"
+   username            = "your_username"
+   password            = "your_password"
   
-#   multi_az = false
+   multi_az = false
 
-# }
+ }
