35b6bf19525920a80749129a61092e669f871399
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-04-30 05:42:45 +0300

    Update 5.1.elb.secgroup.tf

--- Terrafrom/5.1.elb.secgroup.tf
+++ Terrafrom/5.1.elb.secgroup.tf
@@ -2,26 +2,25 @@ resource "aws_security_group" "elb_sg" {
   name   = "alb-security-group"
   vpc_id = aws_vpc.admin_vpc.id  
 
-  # Inbound rules to allow HTTP/HTTPS traffic
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic from anywhere
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic from anywhere
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
-  # Outbound rules for backend communication
+  
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
+    protocol    = "-1"  
     cidr_blocks = ["0.0.0.0/0"]
   }
 
