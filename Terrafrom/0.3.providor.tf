3fa16abb2db5ab8136b43b700d92a61d1223c65a
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-04-30 05:37:43 +0300

    Update 0.3.providor.tf

--- Terrafrom/0.3.providor.tf
+++ Terrafrom/0.3.providor.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXYKJVY5VJOLGA666"
-  secret_key = "LbML7ilyI7MzT4cPd6GEa21wYYNPk3h23D22lNeN"
+  access_key = ""                                         #put_your_access_key
+  secret_key = ""                                         #put_your_secret_key
 }
