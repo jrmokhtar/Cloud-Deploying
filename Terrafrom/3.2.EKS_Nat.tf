49a91f802bd25920e1163500fb22969f08bfede7
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-05-01 21:46:56 +0300

    3.2.EKS_Nat.tf

--- Terrafrom/3.2.EKS_Nat.tf
+++ Terrafrom/3.2.EKS_Nat.tf
@@ -4,11 +4,12 @@ resource "aws_eip" "nat_eip_2" {
 }
 #############################################
 resource "aws_nat_gateway" "nat_gateway3" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.EKS_1thprivate.id
-  depends_on = [aws_internet_gateway.vpc_igw]
+  allocation_id = var.allocation_id
+  subnet_id     = var.subnet_id_1
+  depends_on    = [aws_internet_gateway.vpc_igw]
 }
+
 resource "aws_nat_gateway" "nat_gateway_4" {
-  allocation_id = aws_eip.nat_eip_2.id
-  subnet_id     = aws_subnet.EKS_2thprivate.id
+  allocation_id = var.allocation_id_2
+  subnet_id     = var.subnet_id_2
 }
