0b8fc28b528fc76387b693559a8bb0016c44b99d
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-05-01 21:52:49 +0300

    EKS.tf

--- Terrafrom/3.0.EKS.tf
+++ Terrafrom/3.0.EKS.tf
@@ -37,9 +37,9 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 ########################### EKS CLUSTER  ###################################
 
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "eks-cluster"
-  version = "1.29"
-  role_arn = aws_iam_role.eks_role.arn
+  name        = var.cluster_name
+  version     = var.cluster_version
+  role_arn    = var.role_arn
 
   vpc_config {
     subnet_ids = [
@@ -49,7 +49,6 @@ resource "aws_eks_cluster" "eks_cluster" {
       aws_subnet.EKS_2thpub.id
     ]
   }
-
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_policy,
