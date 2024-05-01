fe08102723f4ab373f30e1269003dccd4a03d1b2
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-05-01 21:59:23 +0300

    s3-elb.tf

--- Terrafrom/5.3.s3-elb.tf
+++ Terrafrom/5.3.s3-elb.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "lblogs" {
-  bucket = "nader-logs"
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_ownership_controls" "lblogs" {
@@ -10,7 +10,6 @@ resource "aws_s3_bucket_ownership_controls" "lblogs" {
   }
 }
 
-
 resource "aws_s3_bucket_lifecycle_configuration" "lblogs" {
   bucket = aws_s3_bucket.lblogs.id
 
@@ -19,24 +18,24 @@ resource "aws_s3_bucket_lifecycle_configuration" "lblogs" {
     status = "Enabled"
 
     expiration {
-      days = 30 
+      days = var.lifecycle_days
     }
   }
 }
+
 ####################################################################
 
 resource "aws_lb" "elb" {
-  name               = "my-elb"
+  name               = var.alb_name
   load_balancer_type = "application"
+
   access_logs {
-    bucket = aws_s3_bucket.lblogs.id
-    prefix = "elb-logs/"
+    bucket = var.log_bucket_id
+    prefix = var.log_prefix
   }
-  subnets            = [
-     aws_subnet.EKS_1thprivate.id,
-     aws_subnet.EKS_2thpub.id
-   ]
-  security_groups    = [aws_security_group.elb_sg.id]
+
+  subnets            = var.alb_subnets
+  security_groups    = var.alb_security_groups
 
   tags = {
     Name = "elb"
