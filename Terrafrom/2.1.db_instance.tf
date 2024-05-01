3fc239e2991382e148fd11e35dcf8b2827f40b75
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-05-01 17:15:23 +0300

    db_instance.tf

--- Terrafrom/2.1.db_instance.tf
+++ Terrafrom/2.1.db_instance.tf
@@ -1,23 +1,21 @@
- resource "aws_db_instance" "RDS_instance" {
- 
-db_subnet_group_name = aws_db_subnet_group.RDS_subnets.name
-   vpc_security_group_ids = [aws_security_group.RDS_secgroup.id]
-   availability_zone = data.aws_availability_zones.AZs.names[0]
+resource "aws_db_instance" "RDS_instance" {
+  db_subnet_group_name   = var.db_subnet_group_name
+  vpc_security_group_ids = [var.security_group_id]
+  availability_zone      = var.availability_zone
   
-   db_name = "rds"
-   license_model = "general-public-license"
-   identifier = "dhangodb"
-
-   allocated_storage    = 20
-   engine              = "mysql"
-   engine_version      = "8.0.35"
-   instance_class      = "db.t3.micro"
-   publicly_accessible = true
-   skip_final_snapshot = true
-
-   username            = "your_username"
-   password            = "your_password"
+  db_name            = "rds"
+  license_model      = "general-public-license"
+  identifier         = "dhangodb"
   
-   multi_az = false
-
- }
+  allocated_storage  = 20
+  engine             = "mysql"
+  engine_version     = "8.0.35"
+  instance_class     = "db.t3.micro"
+  publicly_accessible = true
+  skip_final_snapshot = true
+  
+  username           = var.username
+  password           = var.password
+  
+  multi_az           = false
+}
