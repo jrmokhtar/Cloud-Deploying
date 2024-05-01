375f371b95582902d7450ad9738eadec667a3619
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-05-01 21:45:53 +0300

    3.1.EKS_Subnets.tf

--- Terrafrom/3.1.EKS_Subnets.tf
+++ Terrafrom/3.1.EKS_Subnets.tf
@@ -1,49 +1,38 @@
 resource "aws_subnet" "EKS_1thprivate" {
-  vpc_id            = aws_vpc.admin_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    "Name"                            = "1thprivate"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
-  }
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = var.subnet_tags
 }
+
 #####################################################
 resource "aws_subnet" "EKS_2thprivate" {
-  vpc_id            = aws_vpc.admin_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    "Name"                            = "2thprivate"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
-  }
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = var.subnet_tags
 }
+
 ########################################################
 resource "aws_subnet" "EKS_1thpub" {
-  vpc_id                  = aws_vpc.admin_vpc.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
 
-  tags = {
-    "Name"                       = "1thpub"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
-  }
+  tags = var.subnet_tags
 }
 #########################################################
 resource "aws_subnet" "EKS_2thpub" {
-  vpc_id                  = aws_vpc.admin_vpc.id
-  cidr_block              = "10.0.4.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
 
-  tags = {
-    "Name"                       = "2thpub"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
-  }
+  tags = var.subnet_tags
 }
+
