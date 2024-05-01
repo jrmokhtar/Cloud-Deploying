ba394f15d77ec9b26451007f5b7640b94cbc3f39
Nader Mokhtar <162094566+jrmokhtar@users.noreply.github.com>
2024-05-01 17:12:08 +0300

    jenkins_subnet.tf

--- Terrafrom/1.2.jenkins_subnet.tf
+++ Terrafrom/1.2.jenkins_subnet.tf
@@ -1,13 +1,12 @@
 resource "aws_subnet" "pubadmin_subnet" {
-  vpc_id     = aws_vpc.admin_vpc.id
-  cidr_block = "10.0.10.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
 
-  tags = {
-    Name = "pubadmin_subnet"
-  }
+  tags = var.subnet_tags
 }
+
 
 
 
